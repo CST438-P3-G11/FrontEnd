0c9708f48371adc3553a326a5066daf43e2fefa9
Daniel Everman <daniel@everman.info>
2026-04-22 15:40:41 -0700

    Image now displays below the status bar on mobile

--- app/(tabs)/game.tsx
+++ app/(tabs)/game.tsx
@@ -1,50 +1,52 @@
 import {Animated, StyleSheet, useColorScheme, useWindowDimensions, View} from 'react-native';
-import {MD3DarkTheme, MD3LightTheme, PaperProvider, RadioButton, useTheme} from "react-native-paper";
+import {MD3DarkTheme, MD3LightTheme, PaperProvider, RadioButton} from "react-native-paper";
 import React from "react";
 import AutoHeightImage from "react-native-auto-height-image";
+import {SafeAreaProvider, useSafeAreaInsets} from "react-native-safe-area-context";
 import ScrollView = Animated.ScrollView;
 
 const App = () => {
     const [checked, setChecked] = React.useState('none');
-    const {colors} = useTheme();
     const colorScheme = useColorScheme();
     const theme = colorScheme === 'dark'
         ? {...MD3DarkTheme, colors: {...MD3DarkTheme.colors, onSurface: '#FFFFFF'}}
         : MD3LightTheme;
 
     return (
-        <PaperProvider theme={theme}>
-            <ScrollView contentContainerStyle={styles.container}>
-                <AutoHeightImage
-                    source={{uri: 'https://i.imgur.com/yiT90vB.jpeg'}}
-                    width={useWindowDimensions().width}
-                />
-                <RadioButton.Group onValueChange={checked => setChecked(checked)} value={checked}>
-                    <View style={styles.optionContainer}>
-                        <RadioButton.Item
-                            value="first"
-                            label="Embraer 175"
-                            position="leading"
-                        />
-                        <RadioButton.Item
-                            value="second"
-                            label="Airbus A220"
-                            position="leading"
-                        />
-                        <RadioButton.Item
-                            value="third"
-                            label="Bombardier CRJ-900"
-                            position="leading"
-                        />
-                        <RadioButton.Item
-                            value="fourth"
-                            label="Dassault Falcon 900"
-                            position="leading"
-                        />
-                    </View>
-                </RadioButton.Group>
-            </ScrollView>
-        </PaperProvider>
+        <SafeAreaProvider style={{paddingTop: useSafeAreaInsets().top}}>
+            <PaperProvider theme={theme}>
+                <ScrollView contentContainerStyle={styles.container}>
+                    <AutoHeightImage
+                        source={{uri: 'https://i.imgur.com/yiT90vB.jpeg'}}
+                        width={useWindowDimensions().width}
+                    />
+                    <RadioButton.Group onValueChange={checked => setChecked(checked)} value={checked}>
+                        <View style={styles.optionContainer}>
+                            <RadioButton.Item
+                                value="first"
+                                label="Embraer 175"
+                                position="leading"
+                            />
+                            <RadioButton.Item
+                                value="second"
+                                label="Airbus A220"
+                                position="leading"
+                            />
+                            <RadioButton.Item
+                                value="third"
+                                label="Bombardier CRJ-900"
+                                position="leading"
+                            />
+                            <RadioButton.Item
+                                value="fourth"
+                                label="Dassault Falcon 900"
+                                position="leading"
+                            />
+                        </View>
+                    </RadioButton.Group>
+                </ScrollView>
+            </PaperProvider>
+        </SafeAreaProvider>
     );
 
 }
