20275a450b6b7985c47d3b8b7fe66d047e5658bb
Daniel Everman <daniel@everman.info>
2026-04-22 15:03:24 -0700

    Kind of have something sketched out for the game layout. May have to do away with the AutoHeightImage component but we'll see.

--- app/(tabs)/game.tsx
+++ app/(tabs)/game.tsx
@@ -1,33 +1,65 @@
 import {Image} from 'expo-image';
-import {Animated, Platform, StyleSheet} from 'react-native';
-
+import {Animated, StyleSheet, useWindowDimensions, View} from 'react-native';
+import {RadioButton, useTheme} from "react-native-paper";
+import React from "react";
 import ScrollView = Animated.ScrollView;
+import AutoHeightImage from "react-native-auto-height-image";
 
+const App = () => {
+    const [checked, setChecked] = React.useState('first');
+    const {colors} = useTheme();
 
-
-export default function GameScreen() {
     return (
-        <ScrollView>
-
+        <ScrollView contentContainerStyle={styles.container}>
+            <AutoHeightImage
+                source={{uri: 'https://i.imgur.com/yiT90vB.jpeg'}}
+                width={ useWindowDimensions().width }
+            />
+            <RadioButton.Group onValueChange={checked => setChecked(checked)} value={checked}>
+                <View style={styles.optionContainer}>
+                    <RadioButton.Item
+                        value="first"
+                        label="Embraer 175"
+                        position="leading"
+                        labelStyle={{color: colors.onSurface}}
+                    />
+                    <RadioButton.Item
+                        value="second"
+                        label="Airbus A220"
+                        position="leading"
+                        labelStyle={{color: colors.onSurface}}
+                    />
+                    <RadioButton.Item
+                        value="third"
+                        label="Bombardier CRJ-900"
+                        position="leading"
+                        labelStyle={{color: colors.onSurface}}
+                    />
+                    <RadioButton.Item
+                        value="fourth"
+                        label="Dassault Falcon 900"
+                        position="leading"
+                        labelStyle={{color: colors.onSurface}}
+                    />
+                </View>
+            </RadioButton.Group>
         </ScrollView>
     );
+
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-    titleContainer: {
-        flexDirection: 'row',
+    container: {
+        flex: 1,
+        flexDirection: 'column',
         alignItems: 'center',
-        gap: 8,
     },
-    stepContainer: {
-        gap: 8,
-        marginBottom: 8,
-    },
-    reactLogo: {
-        height: 178,
-        width: 290,
-        bottom: 0,
-        left: 0,
-        position: 'absolute',
-    },
+    optionContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+    }
 });
